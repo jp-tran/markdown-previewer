8ea2e86ee89c415c5a105dbc883b0936e10b3804
ptran <phuoctran27@gmail.com>
2020-09-24 16:13:18 -0700

    Updates

---
+++ precache-manifest.80ffe363b2aa9ebcf0f38d1211302b6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db30d4ef0bed8df1dd321bf17ef2c35",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "5d3f79cab92426436273",
+    "url": "/markdown-previewer/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4fb9d87fa4132c598372",
+    "url": "/markdown-previewer/static/css/main.3b4dd095.chunk.css"
+  },
+  {
+    "revision": "5d3f79cab92426436273",
+    "url": "/markdown-previewer/static/js/2.3f90cfa4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.3f90cfa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb9d87fa4132c598372",
+    "url": "/markdown-previewer/static/js/main.24025d00.chunk.js"
+  },
+  {
+    "revision": "759a7b9334e83da7bf7b",
+    "url": "/markdown-previewer/static/js/runtime-main.58f776fb.js"
+  }
+]);
